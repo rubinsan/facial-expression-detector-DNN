e8c7bf41c4b9952661b9817097bddced7e8a90a9
RobSan <sanchezblazquezruben@gmail.com>
2025-06-23 17:05:57 +0200

    backprop and gradient descent implemented

--- neuralnet.py
+++ neuralnet.py
@@ -3,7 +3,8 @@ TODO: document this module
 """
 
 import numpy as np
-from math_functions import relu, softmax
+import matplotlib.pyplot as plt
+from math_functions import relu, softmax, relu_derivative
 
 class NeuralNet: 
     """
@@ -44,10 +45,19 @@ class NeuralNet:
 
         for epoch in range(epoch_n):
             Z_last, cache = self.forward_prop(X)
-            loss, Y_hat = self.compute_loss(Z_last, Y)
+            cost, Y_hat = self.compute_cost(Z_last, Y)
             grads = self.backward_prop(cache, Y, Y_hat)
-            # update parameters
-            # plot loss vs epoch number
+            self.gradient_descent(grads, learning_rate)
+            if epoch % 100 == 0:
+                print("Cost after epoch:", epoch, ":", cost)
+
+            """  
+            plt.plot(cost)
+            plt.ylabel('cost')
+            plt.xlabel('epochs (per hundreds)')
+            plt.title("Learning rate =" + str(learning_rate))
+            plt.show()
+            """
     
     def forward_prop(self, X):
         """
@@ -63,17 +73,18 @@ class NeuralNet:
         cache = {}
         Z = []
         A = X
+        cache["A0"] = X # store for backpropagation
         for i in range(len(self.dims) - 1):
             W = self.weights["W"+str(i+1)]
             Z = np.matmul(W, A) + self.biases["b"+str(i+1)]
             A = relu(Z)
+            if (i+1) >= (len(self.dims) - 1): continue  # last layer not stored
             cache["Z"+str(i+1)] = Z
             cache["A"+str(i+1)] = A 
-        cache.pop("A"+str(len(self.dims)-1), None) # last layer act is not relu
         Z_last = Z  # Last layer output before activation
         return Z_last, cache
 
-    def compute_loss(self, Z_last, Y):
+    def compute_cost(self, Z_last, Y):
         """
         Compute the loss using cross-entropy loss function.
 
@@ -85,12 +96,11 @@ class NeuralNet:
         loss -- computed loss value
         """
         Y_hat = softmax(Z_last)
-        print(Y_hat)
         Y_hat_vector = np.sum((Y*Y_hat), axis=0, keepdims=True)
         loss_vector = -np.log(Y_hat_vector)
-        loss = np.sum(loss_vector) / Y.shape[1]
+        cost = np.sum(loss_vector) / Y.shape[1]
 
-        return loss, Y_hat
+        return cost, Y_hat
         
     def backward_prop(self, cache, Y, Y_hat):
         """
@@ -104,21 +114,42 @@ class NeuralNet:
         gradients -- dictionary containing gradients for weights and biases
         """
         gradients = {}
+        m = 1/Y.shape[1] # number of samples
         
+        # last layer calculations, different activation function (softmax)
         dZ = Y_hat-Y # first dZ
-        dW = (1/Y.shape[1]) * (np.matmul(dZ, 
-                                         cache["A"+str(len(self.dims)-1)].T)) 
-        
+        dW = m * np.matmul(dZ, cache["A"+str(len(self.dims)-2)].T)
+        db = m * np.sum(dZ, axis=1, keepdims=True)
+        dA_prev = np.matmul(self.weights["W"+str(len(self.dims)-1)].T, dZ)
+        gradients["dW"+str(len(self.dims)-1)] = dW
+        gradients["db"+str(len(self.dims)-1)] = db
+        cache.pop("A"+str(len(self.dims)-2), None)
 
-
-        for i in reversed(range(len(self.dims) - 2)):
-            #dG = relu_derivative()
-            pass
-
-        
-
+        for i in reversed(range(len(self.dims) - 2)): # loop from last - 1 layer
+            dZ = dA_prev * relu_derivative(cache["Z"+str(i+1)]) 
+            dW = m * np.matmul(dZ, cache["A"+str(i)].T)
+            db = m * np.sum(dZ, axis=1, keepdims=True)
+            dA_prev = np.matmul(self.weights["W"+str(i+1)].T, dZ)
+            cache.pop("Z"+str(i+1), None) # release memory
+            cache.pop("A"+str(i), None) # release memory
+            gradients["dW"+str(i+1)] = dW
+            gradients["db"+str(i+1)] = db
+            
         return gradients
 
+    def gradient_descent(self, gradients, learning_rate):
+        """
+        Update the weights and biases using gradient descent.
+
+        Parameters:
+        gradients -- dictionary containing gradients for weights and biases
+        learning_rate -- learning rate for the update step
+        """
+        for i in range(len(self.dims) - 1):
+            self.weights["W"+str(i+1)] -= learning_rate * gradients["dW"+str(i+1)]
+            self.biases["b"+str(i+1)] -= learning_rate * gradients["db"+str(i+1)]
+            gradients.pop("dW"+str(i+1), None) # release memory
+            gradients.pop("db"+str(i+1), None) # release memory
 
     def predict(self, X, Y):
         pass
--- unit_tests.py
+++ unit_tests.py
@@ -49,7 +49,7 @@ print(Z)
 Y = np.eye(n_classes)[np.random.choice(n_classes, batch_size)]
 Y = Y.T
 print(Y)
-loss = test_NN.compute_loss(Z, Y)
+loss = test_NN.compute_cost(Z, Y)
 print("loss output:", loss)
 """
 
@@ -65,11 +65,11 @@ Z_last, cache = test_NN.forward_prop(X)
 for i in range(len(test_NN.dims) - 1):
     print("W"+str(i+1)+":")
     print(test_NN.weights["W"+str(i+1)])
+    if (i+1) >= (len(test_NN.dims) - 1): continue
     print("Z"+str(i+1)+":")
     print(cache["Z"+str(i+1)])
-    if i < len(test_NN.dims) - 2:
-        print("A"+str(i+1)+":")
-        print(cache["A"+str(i+1)])
+    print("A"+str(i+1)+":")
+    print(cache["A"+str(i+1)])
 """
 
 # Test of relu_derivative function
@@ -86,4 +86,24 @@ for i in range(len(test_NN.dims) - 1):
     print(cache["Z"+str(i+1)])
     print("dG"+str(i+1)+":")
     print(relu_derivative(cache["Z"+str(i+1)]))
+"""
+
+# Test backward_prop of NeuralNet class
+"""
+test_NN = NeuralNet([6, 6, 4, 5, 6])
+test_NN.initialization()
+batch_size = 4
+X = np.random.rand(test_NN.dims[0], batch_size) * 100
+print("X:")
+print(X)
+Z_last, cache = test_NN.forward_prop(X)
+Y = np.eye(test_NN.dims[-1])[np.random.choice(test_NN.dims[-1], batch_size)]
+Y = Y.T
+loss, Y_hat = test_NN.compute_cost(Z_last, Y)
+grads = test_NN.backward_prop(cache, Y, Y_hat)
+for i in reversed(range(len(test_NN.dims) - 1)):
+    print("dW"+str(i+1)+":")
+    print(grads["dW"+str(i+1)])
+    print("db"+str(i+1)+":")
+    print(grads["db"+str(i+1)])
 """
