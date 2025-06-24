0a0562edca533e170141bea3dbdbf893813224de
RobSan <sanchezblazquezruben@gmail.com>
2025-06-24 19:08:57 +0200

    predict method coded and tested

--- neuralnet.py
+++ neuralnet.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0e138161dc3f28b132f8b339960e1fdd7f7fc415654938b3a9b9dae3876b08dd
-size 5223
+oid sha256:9620e856e1d9bb385b6b607de2a7b20f159623eb0e5a37eca4ef3e9cc5c7d8fa
+size 5826
--- neuralnet_test.py
+++ neuralnet_test.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8e8792ee13bfa6d56df0f6d9c5fe9fdaadf785b11127a7ed1405a760ddbfe8c2
-size 1453
+oid sha256:3cfeb04b82201a7d4960e911990921cb36b8213e897869a05a090a9a4050baeb
+size 2035
