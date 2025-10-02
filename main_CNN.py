2687a3269bd83c5afb28c2c598eb2f0a92c9a02e
rubinsan <sanchezblazquezruben@email.com>
2025-10-02 15:09:39 +0200

    first CNN implemented with pytorch, low accuracy, different architecture is needed

--- main_CNN.py
+++ main_CNN.py
@@ -1,1 +1,145 @@
-# Test github connection with Jetson
+# Pytorch implementation for facial expression recognition on 
+# manojdilz/facial_emotion_detection_dataset using CNN
+# Author Ruben Sanchez - github/rubinsan
+
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
+# Download training data from open datasets.
+training_data = datasets.FER2013(
+    root="data_FER2013",
+    split="train",
+    transform=ToTensor(), 
+)
+
+# Download test data from open datasets.
+test_data = datasets.FER2013(
+    root="data_FER2013",
+    split="test",
+    transform=ToTensor(), 
+)
+
+batch_size = 64
+
+# Create data loaders.
+train_dataloader = DataLoader(training_data, batch_size=batch_size)
+test_dataloader = DataLoader(test_data, batch_size=batch_size)
+
+for X, y in train_dataloader:
+    print(f"Shape of X [N, C, H, W]: {X.shape}")
+    print(f"Shape of y: {y.shape} {y.dtype}")
+    print(y[0:5])
+    break
+
+device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
+print(f"Using {device} device")
+
+# Define model
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super(NeuralNetwork, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(1, 32, 3, stride=1, padding='same'),
+            nn.ReLU(),
+            nn.Conv2d(32, 32, 3, stride=1, padding='same'),
+            nn.ReLU(),
+            nn.Conv2d(32, 32, 3, stride=1, padding='same'),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2)
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(32, 64, 3, stride=1, padding='same'),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, 3, stride=1, padding='same'),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, 3, stride=1, padding='same'),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2)
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(64, 128, 3, stride=1, padding='same'),
+            nn.ReLU(),
+            nn.Conv2d(128, 128, 3, stride=1, padding='same'),
+            nn.ReLU(),
+            nn.Conv2d(128, 128, 3, stride=1, padding='same'),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+        )   
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(128, 256, 3, stride=1, padding='same'),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, 3, stride=1, padding='same'),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, 3, stride=1, padding='same'),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Dropout2d(p=0.2)
+        )  
+        self.fc = nn.Sequential(    
+            nn.Flatten(),
+            nn.Linear(2304, 256),
+            nn.ReLU(),
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.Linear(128, 7)
+        )
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        logits = self.fc(x)
+        return logits
+
+model = NeuralNetwork().to(device)
+print(model)
+
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+
+def train(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        X, y = X.to(device), y.to(device)
+
+        # Compute prediction error
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), (batch + 1) * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+def test(dataloader, model, loss_fn):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.4f}%, Avg loss: {test_loss:>8f} \n")
+
+epochs = 20
+for t in range(epochs):
+    print(f"Epoch {t+1}\n-------------------------------")
+    train(train_dataloader, model, loss_fn, optimizer)
+    test(test_dataloader, model, loss_fn)
+print("Done!")
+
