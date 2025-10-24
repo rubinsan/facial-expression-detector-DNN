8325b8a883f624f9c94893565b5d6c52cb842fcf
rubinsan <sanchezblazquezruben@email.com>
2025-10-24 20:22:11 +0200

    ResNet18 implementation, overfitting after first test

--- main_CNN_VGG19.py
+++ main_CNN_VGG19.py
@@ -1,5 +1,5 @@
 # Pytorch implementation for facial expression recognition on 
-# manojdilz/facial_emotion_detection_dataset using CNN
+# FER2013 dataset using CNN
 # Author Ruben Sanchez - github/rubinsan
 
 import torch
@@ -122,10 +122,10 @@ model = NeuralNetwork().to(device)
 print(model)
 
 loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=0.002, momentum=0.9, 
-                            weight_decay=9e-4, nesterov=True)
-#scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
-#            optimizer, mode='max', factor=0.75, patience=5)
+optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9, 
+                            weight_decay=1e-4, nesterov=True)
+scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+            optimizer, mode='max', factor=0.75, patience=5)
 
 def train(dataloader, model, loss_fn, optimizer):
     size = len(dataloader.dataset)
@@ -160,11 +160,13 @@ def test(dataloader, model, loss_fn):
     test_loss /= num_batches
     correct /= size
     print(f"Test Error: \n Accuracy: {(100*correct):>0.4f}%, Avg loss: {test_loss:>8f} \n")
+    return correct
 
-epochs = 20
+epochs = 30
 for t in range(epochs):
     print(f"Epoch {t+1}\n-------------------------------")
     train(train_dataloader, model, loss_fn, optimizer)
-    test(test_dataloader, model, loss_fn)
+    val_acc = test(test_dataloader, model, loss_fn)
+    scheduler.step(val_acc)
 print("Done!")
 
